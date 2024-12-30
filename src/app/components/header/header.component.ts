54258bf0a5a516135f21e9904b97e3740bbdbf38
Maryam-Mehzabin-Zahin <zahin.maryam.14@gmail.com>
2024-12-29 18:27:09 -0600

    change html

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = signal('Maryam Sapce');
+  title = signal("Maryam's Space");
 }
