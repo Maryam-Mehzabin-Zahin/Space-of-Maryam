26d877d55184eb60c05303502e0aa175502b248b
Maryam-Mehzabin-Zahin <zahin.maryam.14@gmail.com>
2024-12-30 18:35:13 -0600

    create button with input and output

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+showBtnClicked() {
+console.log('clicked');
+}
   title = signal("Maryam's Space");
 }
