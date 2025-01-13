64189f7adf81f6336673f9088ee04362a6ceca8f
Maryam-Mehzabin-Zahin <zahin.maryam.14@gmail.com>
2025-01-13 08:47:36 -0600

    add contact info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         component: ProductsListComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
     }
 ];
